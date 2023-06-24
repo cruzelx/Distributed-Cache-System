b1e586d83a3484b47f58985f1d5e029b29b16020
Alex Bhattarai <cruze.alex@gmail.com>
2023-06-24 23:54:04 +0545

    Implement function to load and store cache from and to disk

--- auxiliary/LRU.go
+++ auxiliary/LRU.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
+	"os"
 )
 
 type LRU struct {
 	capacity int
 	bucket   map[string]*Node
 	dll      *DLL
+	filepath string
 }
 
-func NewLRU(capacity int) *LRU {
+func NewLRU(capacity int, filepath string) *LRU {
 	return &LRU{
 		capacity: capacity,
 		bucket:   make(map[string]*Node, capacity),
 		dll:      NewDLL(),
+		filepath: filepath,
 	}
 }
 
@@ -40,5 +44,60 @@ func (lru *LRU) Put(key string, value string) {
 		lru.bucket[key] = newNode
 		lru.dll.Prepend(newNode)
 	}
+
+}
+
+func (lru *LRU) saveToDisk() (bool, error) {
+
+	file, err := os.OpenFile(lru.filepath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0777)
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	if len(lru.bucket) == 0 {
+		return true, nil
+	}
+
+	curr := lru.dll.Head
+
+	temp := make(map[string]string)
+
+	for curr != nil {
+		temp[curr.Key] = curr.Value
+		curr = curr.Next
+	}
+
+	encode := gob.NewEncoder(file)
+	if err := encode.Encode(temp); err != nil {
+		return false, err
+	}
+	fmt.Println("saved at: ", lru.filepath)
+	return true, nil
+}
+
+func (lru *LRU) loadFromDisk() (bool, error) {
+	file, err := os.Open(lru.filepath)
+	defer file.Close()
+	if err != nil {
+		return os.IsNotExist(err), err
+	}
+
+	decode := gob.NewDecoder(file)
+
+	temp := make(map[string]string)
+
+	if err := decode.Decode(&temp); err != nil {
+		return false, err
+	}
+
+	lru.dll = NewDLL()
+
+	for k, v := range temp {
+		node := &Node{Key: k, Value: v}
+		lru.dll.Append(node)
+		lru.bucket[k] = node
+	}
+	return true, err
 
 }
