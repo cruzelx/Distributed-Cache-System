9197c209fb2a7f7001621974e4c85ac9ed3790e3
Alex Bhattarai <cruze.alex@gmail.com>
2023-06-26 13:08:39 +0545

    test key distribution

--- master/master_test.go
+++ master/master_test.go
@@ -64,17 +64,31 @@ func TestHashRing_GetNode(t *testing.T) {
 }
 
 func TestHashRing_Performance(t *testing.T) {
-	hr := NewHashRing(3)
+	hr := NewHashRing(10)
 
 	hr.AddNode("aux1:3001")
 	hr.AddNode("aux2:3002")
 	hr.AddNode("aux3:3003")
+
+	count := map[string]int{
+		"aux1:3001": 0,
+		"aux2:3002": 0,
+		"aux3:3003": 0,
+	}
 
 	numOfKeys := 10_00_00_00
 	keys := make([]string, numOfKeys)
 
 	for i := 0; i < numOfKeys; i++ {
 		keys[i] = fmt.Sprintf("key%d", i)
+	}
+
+	for _, key := range keys {
+		node, err := hr.GetNode(key)
+		if err != nil {
+			t.Errorf("Failed to get map for the key %s : err %v", key, err)
+		}
+		count[node] += 1
 	}
 
 	startTime := time.Now()
@@ -85,12 +99,15 @@ func TestHashRing_Performance(t *testing.T) {
 		}
 
 	}
-
 	elapsedTime := time.Since(startTime)
 
 	t.Logf("Performance test: GetNode for %d keys", numOfKeys)
 	t.Logf("Elapsed time: %s", elapsedTime)
 	t.Logf("Average time per key: %s", elapsedTime/time.Duration(numOfKeys))
+	t.Logf("Distribution: aux1:3001 => %.2f%% aux2:3002 => %.2f%% aux3:3003 => %.2f%% ",
+		float64(count["aux1:3001"])*100.0/float64(numOfKeys),
+		float64(count["aux2:3002"])*100.0/float64(numOfKeys),
+		float64(count["aux3:3003"])*100.0/float64(numOfKeys))
 }
 
 func TestMain(m *testing.M) {
