4fc8ee91cdda52ca04c032bc903fc907e9a82993
Alex Bhattarai <cruze.alex@gmail.com>
2023-07-06 23:18:50 +0545

    function to send mappings to master server

--- auxiliary/controller.go
+++ auxiliary/controller.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -62,4 +66,52 @@ func (aux *Auxiliary) Mappings(w http.ResponseWriter, r *http.Request) {
 func (aux *Auxiliary) Erase(w http.ResponseWriter, r *http.Request) {
 	aux.LRU.EraseCache()
 	w.WriteHeader(http.StatusOK)
+}
+
+func (aux *Auxiliary) Health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func (aux *Auxiliary) SendMappings() {
+
+	keyvals := map[string]string{}
+	curr := aux.LRU.dll.Head
+
+	for curr != nil {
+		keyvals[curr.Key] = curr.Value
+		curr = curr.Next
+	}
+
+	postBody, err := json.Marshal(keyvals)
+	if err != nil {
+		log.Printf("couldn't parse key-val pairs: %v\n", err)
+		return
+	}
+
+	log.Println("Sending mappings to master server...")
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "http://master:8000/rebalance-dead-aux", bytes.NewBuffer(postBody))
+	if err != nil {
+		log.Println("couldn't create the request")
+		return
+	}
+
+	port := os.Getenv("PORT")
+	serverId := os.Getenv("ID")
+
+	auxServer := fmt.Sprintf("%s:%s", serverId, port)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("aux-server", auxServer)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("couldn't send mappings to master server")
+		return
+	}
+	defer resp.Body.Close()
+
+	log.Println("Mappings sent!!!")
+
 }
