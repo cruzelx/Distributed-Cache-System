a6b15bef8a6ee8b0e3d5eb233dd1cf99edb8ead8
Alex Bhattarai <cruze.alex@gmail.com>
2023-07-19 01:53:25 +0545

    request counter and response time metric on get and put controllers

--- auxiliary/controller.go
+++ auxiliary/controller.go
@@ -7,12 +7,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Auxiliary struct {
-	LRU *LRU
+	LRU          *LRU
+	requests     *prometheus.CounterVec
+	responseTime *prometheus.HistogramVec
 }
 
 type KeyVal struct {
@@ -20,7 +24,36 @@ type KeyVal struct {
 	Value string `json:value`
 }
 
+func NewAuxiliary(bucketSize int, filepath string) *Auxiliary {
+	requests := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "auxiliary_request_total",
+			Help: "Total number of requests to the auxiliary node",
+		}, []string{"method"},
+	)
+
+	responseTime := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "auxiliary_response_time_seconds",
+			Help:    "Distribution of the response time processed by the auxiliary server",
+			Buckets: prometheus.ExponentialBuckets(0.001, 2, 16),
+		},
+		[]string{"method"},
+	)
+
+	prometheus.MustRegister(requests, responseTime)
+
+	return &Auxiliary{
+		LRU:          NewLRU(bucketSize, filepath),
+		requests:     requests,
+		responseTime: responseTime,
+	}
+
+}
+
 func (aux *Auxiliary) Put(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+
 	var kv KeyVal
 
 	if err := json.NewDecoder(r.Body).Decode(&kv); err != nil {
@@ -30,11 +63,16 @@ func (aux *Auxiliary) Put(w http.ResponseWriter, r *http.Request) {
 
 	aux.LRU.Put(kv.Key, kv.Value)
 
+	elapsedTime := time.Since(startTime).Seconds()
+	aux.requests.WithLabelValues(r.Method).Inc()
+	aux.responseTime.WithLabelValues(r.Method).Observe(elapsedTime)
 	w.WriteHeader(http.StatusOK)
 
 }
 
 func (aux *Auxiliary) Get(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+
 	vars := mux.Vars(r)
 	key := vars["key"]
 
@@ -44,9 +82,12 @@ func (aux *Auxiliary) Get(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
+	elapsedTime := time.Since(startTime).Seconds()
+	aux.requests.WithLabelValues(r.Method).Inc()
+	aux.responseTime.WithLabelValues(r.Method).Observe(elapsedTime)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(KeyVal{Key: key, Value: val})
-
 }
 
 func (aux *Auxiliary) Mappings(w http.ResponseWriter, r *http.Request) {
@@ -88,11 +129,11 @@ func (aux *Auxiliary) SendMappings() {
 		return
 	}
 
-	log.Println("Sending mappings to master server...")
+	log.Println("sending mappings to master server...")
 
 	client := &http.Client{}
-	masterPort := os.Getenv("MASTER_PORT")
-	req, err := http.NewRequest("POST", fmt.Sprintf("http://master:%s/rebalance-dead-aux", masterPort), bytes.NewBuffer(postBody))
+	masterServer := os.Getenv("MASTER_SERVER")
+	req, err := http.NewRequest("POST", fmt.Sprintf("http://%s/rebalance-dead-aux", masterServer), bytes.NewBuffer(postBody))
 	if err != nil {
 		log.Println("failed to create the request")
 		return
@@ -108,11 +149,11 @@ func (aux *Auxiliary) SendMappings() {
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("failed to send mappings to master server")
+		log.Printf("failed to send mappings to master server: %s", err.Error())
 		return
 	}
 	defer resp.Body.Close()
 
-	log.Println("Mappings sent!!!")
+	log.Println("mappings sent!!!")
 
 }
